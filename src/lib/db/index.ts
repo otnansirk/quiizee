5332f790b26ff8aa56be70ffb591c9445bba669b
otnansirk <iam.otnansirk@gmail.com>
2026-07-10 16:18:22 +0700

    fix: postgresClient on prod

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -10,13 +10,18 @@ const globalForDb = globalThis as unknown as {
 
 const client = globalForDb.postgresClient ?? postgres(connectionString, {
   prepare: false,
-  max: 5,
-  idle_timeout: 20,
+  fetch_types: false,
+  max: 3,
+  idle_timeout: 5,
   connect_timeout: 10,
+  max_lifetime: 60,
+  onclose: function (connId) {
+    // Suppress unhandled close errors
+  },
 });
 
-if (process.env.NODE_ENV !== 'production') {
+// if (process.env.NODE_ENV !== 'production') {
   globalForDb.postgresClient = client;
-}
+// }
 
 export const db = drizzle(client, { schema });
