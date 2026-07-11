b92f4cfa41b0f2fe65b7d9063f5ffa6f2058ed46
otnansirk <iam.otnansirk@gmail.com>
2026-07-11 13:31:13 +0700

    refactor: setup tailwind and global styles

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  webpack(config) {
+    config.watchOptions = {
+      ignored: ['**/node_modules', '**/.git', '**/.open-next'],
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
